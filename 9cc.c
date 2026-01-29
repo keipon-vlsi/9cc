418c7872d219bd36ff7a61fec640e0dd10b91875
Kei Awano <awano.kei.kyoto@gmail.com>
2026-01-29 17:46:54 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -25,11 +25,26 @@ struct Token {
 //現在注目しているトークン
 Token *token;
 
+char *user_input;
+
 //エラーを報告するための関数
 //printfと同じ引数をとる
 void error(char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); //pos個の空白を出力
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -46,14 +61,14 @@ bool consume(char op) {
 //次のトークンが期待している記号の時は、トークンを1つ読み進める。それ以外の場合にはエラーを報告する。
 void expect(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
 //次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。それ以外の場合にはエラーを報告する。
 int expect_number() {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -108,6 +123,8 @@ int main(int argc, char **argv) {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
+
+    user_input = argv[1];
 
     //トークナイズする
     token = tokenize(argv[1]);
